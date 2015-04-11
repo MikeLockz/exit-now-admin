54078d9fbdd5096927780931103b2875ea9d9b93
Michael Lockwitz <mike@lockdev.com>
2015-04-11 12:43:23 -0600

    updating views and sample data

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ var homeController = require('./controllers/home');
 var userController = require('./controllers/user');
 var apiController = require('./controllers/api');
 var contactController = require('./controllers/contact');
+var newDealController = require('./controllers/newDeal');
+var dashboardController = require('./controllers/dashboard');
 
 /**
  * API keys and Passport configuration.
@@ -109,6 +111,8 @@ app.post('/account/profile', passportConf.isAuthenticated, userController.postUp
 app.post('/account/password', passportConf.isAuthenticated, userController.postUpdatePassword);
 app.post('/account/delete', passportConf.isAuthenticated, userController.postDeleteAccount);
 app.get('/account/unlink/:provider', passportConf.isAuthenticated, userController.getOauthUnlink);
+app.post('/new-deal', passportConf.isAuthenticated, newDealController.postDeal);
+app.get('/dashboard', passportConf.isAuthenticated, dashboardController.getDashboard);
 
 /**
  * API examples routes.
